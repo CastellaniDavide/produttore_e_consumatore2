069cdacfd16b18f800cb26b7feb91fb6af436207
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-04-27 20:24:24 +0200

    Created choco/tools/chocolateyinstall.ps1

---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿# Variabiles
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ppath = "C:\Progra~1\produttore_e_consumatore2"
+$path = "$ppath\__init__"
+echo "My personal path is: $ppath"
+echo "My exe path id $exepath"
+
+# Set folder and rename exe file
+New-Item -ItemType Directory -Force -Path $ppath
+Copy-Item -Path "$toolsDir\dist\__init__\*" -Destination "$ppath\" -Recurse
+Rename-Item -Path "$ppath\__init__.exe" -NewName "$ppath\produttore_e_consumatore2.exe"
+
+# Add Path
+$new_path = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+if ($new_path -notlike "*$ppath*")
+{
+	$new_path += ";$ppath"
+	$new_path = $new_path.Replace(";;", ";")
+	Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $new_path
+}
