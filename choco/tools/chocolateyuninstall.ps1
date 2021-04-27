d3d7f82afc0098060f77a39c287c87bcf9d56efd
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-04-27 20:24:20 +0200

    Created choco/tools/chocolateyuninstall.ps1

---
+++ choco/tools/chocolateyuninstall.ps1
@@ -0,0 +1,18 @@
+﻿# My personal variabiles
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ppath = "C:\Progra~1\produttore_e_consumatore2"
+$exepath = "$ppath\produttore_e_consumatore2.exe"
+echo "My personal path is: $ppath"
+echo "My exe path id $exepath"
+
+# Set folder
+Remove-Item -LiteralPath $ppath -Force -Recurse
+
+# Add Path
+$new_path = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
+if ($new_path -like "*$ppath*")
+{
+	echo "Delate unwanted path"
+	$new_path = $new_path.Replace("$ppath", "").Replace(";;", ";")
+	Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $new_path
+}
