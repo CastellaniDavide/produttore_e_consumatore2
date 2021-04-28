3e65aaae59e082a8af8937664c8ab405008e155c
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-04-28 16:08:15 +0200

    v01.01 # 2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Lastest release
     version=get(f"{gh_api}/tags").json()[0]['name'].replace("v", ""),
     description=get(gh_api).json()['description'],
-    long_description=get("{gh_raw}/main/docs/README.md").text,
+    long_description=get(f"{gh_raw}/main/docs/README.md").text,
     long_description_content_type="text/markdown",
     url=get(gh_api).json()['html_url'],
     author=get(gh_api).json()['owner']['login'],
@@ -22,7 +22,7 @@ setup(
     platforms="linux_distibution",
     install_requires=[
         i
-        for i in get("{gh_raw}/main/requirements/requirements.txt").text.split(
+        for i in get(f"{gh_raw}/main/requirements/requirements.txt").text.split(
             "\n") if "#" not in i and i != ''],
     zip_safe=True
 )
